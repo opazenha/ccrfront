51d41fd3d20b351400265b5c4386d70983b6d48d
opazenha <lucaszenh@gmail.com>
2025-03-06 10:34:25 +0000

    Added style to global CSS file. Added login screen for remote testing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,58 @@
+import { NgIf } from '@angular/common';
+import { HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+interface PrayerRequest {
+  name: string;
+  email: string;
+  request: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ccrfront';
+  title = 'Prayer Request Form';
+  
+  prayerRequest: PrayerRequest = {
+    name: '',
+    email: '',
+    request: ''
+  };
+  
+  submitted = false;
+  http: any;
+
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      console.log('Prayer request submitted:', this.prayerRequest);
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: 'Basic ' + btoa('daniel:daniel')
+        })
+      };
+      this.http.post('http://192.168.1.123:7771/api/v1/prayers/', this.prayerRequest, httpOptions).subscribe((response: any) => {
+        console.log('Prayer request posted successfully:', response);
+      });
+      
+      this.submitted = true;
+      
+      // Reset the form after submission
+      setTimeout(() => {
+        this.prayerRequest = {
+          name: '',
+          email: '',
+          request: ''
+        };
+        form.resetForm();
+        this.submitted = false;
+      }, 5000);
+    }
+  }
 }
